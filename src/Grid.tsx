4bb515eb1398b01932424b2ca3fdba6b9a41079c
Antonio <tonymagia007@gmail.com>
2023-11-24 15:29:37 -0500

    Task #6

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,4 +1,4 @@
-import {useMemo} from "react";
+import { useMemo, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
@@ -25,15 +25,26 @@ const columnDefs: ColDef[] = [
 
 const NeoGrid = (): JSX.Element => {
 
+  const gridRef = useRef<AgGridReact>(null);
+
   const defaultColDef = useMemo(()=>({
     sortable: true
   }), []);
 
+  const resetAll = () => {
+    if (gridRef.current) {
+      gridRef.current.api.setFilterModel({});
+      gridRef.current.columnApi.resetColumnState();
+    }
+  }
+
   return (
     <>
       <h1 style={{display: "inline", marginRight: "15px"}}>Near-Earth Object Overview</h1>
+      <button onClick={resetAll}>Clear Filters and Sorters</button>
       <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-        <AgGridReact
+        <AgGridReact          
+          ref={gridRef}
           rowData={data}
           columnDefs={columnDefs}
           defaultColDef={defaultColDef}
